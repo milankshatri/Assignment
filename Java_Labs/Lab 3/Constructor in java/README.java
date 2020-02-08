0f934cd6a781f011ada126015b1e2c5a34aaeb38
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-08 18:52:23 +0545

    Update README.java

--- Java_Labs/Lab 3/Constructor in java/README.java
+++ Java_Labs/Lab 3/Constructor in java/README.java
@@ -1,1 +1,43 @@
+# Constructor in Java
 
+In java, a ```Constructor``` is a block of codes similar to method. It is called when instance of class i.e object is created.
+
+* In java the name of method and class is same.
+* Constructor doesn't have return type not even void
+* A java construcor cannot be abstract, static, final and synchronized
+
+__There are two types of Constructor in java__
+
+1. Default constructor(No-arg)
+1. Parameterized Constructor
+
+
+
+
+1. Default constructor 
+
+* Example
+```java
+class Example{
+	Example(){
+		//declaratio
+	}
+	public static void main(String[] args) {
+		//calling a default constructor
+		Example object=new Example();
+	}
+}
+```
+1. Parmeterized Constructor
+* Example
+```java
+class Example{
+	Example(local variable){
+		//declaration
+	}
+	public static void main(String[] args) {
+		//calling a parameterized constructor
+		Example object=new Example(1,2);
+	}
+}
+```
